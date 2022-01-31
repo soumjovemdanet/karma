e72cdd1b98533dfdab88e5259c9eff8223b94ec0
soumjovemdanet <96385166+soumjovemdanet@users.noreply.github.com>
2022-01-31 18:34:02 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import time 
+
+print("por favor aguarde 5 segundos"
+
+time.sleep(5)
+      
+      
+  Print("Estamos instalando aguarde 10 minutos")
+      
+      os.system("apt-update")
+      os.system("apt upgrade"
+      
+      os.system("pip install --upgrade pip")
+      os.system("pip3 install --upgrade pip")
+time.sleep(1)
+                
+                
+                Print("Muito obrigado por usar o script"
+
+           
